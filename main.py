8f8713053e4451f8afd7ad9c919913a029882f4a
Parsa Tavakoli <parsatavakoli12@gmail.com>
2022-10-15 14:48:30 +0100

    Forest and going into second screen

--- main.py
+++ main.py
@@ -2,38 +2,46 @@ import pygame
 
 pygame.init()
 
-WIDTH, HEIGHT = 1280, 720
-WIN = pygame.display.set_mode((WIDTH,HEIGHT))
-pygame.display.set_caption('Very Accessible Game')
+WIDTH, HEIGHT = 1280, 720 #size of window
+WIN = pygame.display.set_mode((WIDTH,HEIGHT)) #set window size
+pygame.display.set_caption('Very Accessible Game') #title of game
 
-font = pygame.font.Font('freesansbold.ttf', 32)
+font = pygame.font.Font('freesansbold.ttf', 32) #font used for the game
+
+imp = pygame.image.load("forest.jpg").convert()
+WIN.blit(imp, (0, 0)) #displays the image
+pygame.display.flip()
 
 
 yes = font.render('yes', True, (0,0,0))
-yesRect = yes.get_rect()
+yesRect = yes.get_rect() #yes writing
 yesRect.center = (WIDTH// 2, HEIGHT// 2)
 
 no = font.render('no', True, (0,0,0))
-noRect = no.get_rect()
+noRect = no.get_rect() # no writing
 noRect.center = (WIDTH// 2, HEIGHT// 2)
 
-welcome = font.render('Welcome to Game', True, (0,255,0))
-welcRect = welcome.get_rect()
+welcome = font.render('Accessible Dungeon Game. Press Anywhere to start', True, (255,255,255))
+welcRect = welcome.get_rect() #home screen welcome writing
 welcRect.center = (WIDTH// 2, HEIGHT// 2)
 
 def main():
 
     run = True
+    pygame.draw.rect(WIN, (0,0,0), pygame.Rect((WIDTH// 2) - 500 , (HEIGHT// 2)- 30, 1000, 60)) #dispkay rectangle
     WIN.blit(welcome, welcRect)
+    
+
     while run:
-        for event in pygame.event.get():
+        for event in pygame.event.get(): #listen for events
             
             
-            if event.type == pygame.QUIT:
+            if event.type == pygame.QUIT: #if quit end game
                 run = False
-            elif event.type == pygame.MOUSEBUTTONDOWN:
+            elif event.type == pygame.MOUSEBUTTONDOWN: #left click goes to next screen
                 if event.button == 1:
-                    WIN.fill((0,255,0))
+                    WIN.blit(imp, (0, 0))
+                    pygame.display.flip() 
                     WIN.blit(yes, yesRect)
 
                 elif event.button == 3:
